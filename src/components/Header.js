8f25600787e2490800eba5fe81af04182d4a8a7c
KostArt <94619605+ArtemKostuchenko@users.noreply.github.com>
2022-12-04 11:02:34 +0200

    artem_branch
    * Added Main and Header Components with style CSS
    
    * Added Main and Header Components

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import compare from "../images/compare-header.png";
+import heart from "../images/heart-header.png";
+import cart from "../images/cart-header.png";
+
+
+function Header() {
+    return (
+        <header>
+            <div className="brand">FakeStore</div>
+            <div className="navigation">
+                <div>
+                    <button>
+                        <img src={compare} width={30} alt="Compare"/>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src={heart} width={30} alt="Heart"/>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src={cart} alt="Cart" width={30}/>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
