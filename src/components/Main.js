a9d92a5e1318d25e7696e1c43c5f06c12c5e14d8
PrograMist <ipz204_kav@student.ztu.edu.ua>
2022-12-04 14:11:03 +0200

    Added getting products from the site or from local storage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
+import {fetchData} from "../utils/Others";
 
 
 function Main() {
+    //states
+    const [products, setProducts] = useState([]);
+
+
+    //get
+
+    const getProducts = async () => {
+        let data;
+        data = await fetchData('https://fakestoreapi.com/products');
+        if (data){
+            setProducts(data);
+        }
+    }
+
+    //useEffects
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+
     return (
         <div>
             <Header/>
+
         </div>
     )
 }
